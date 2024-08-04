0e93880c8bd3ef169a63affc98f0622433630ff9
AhmedX404 <ahmmedd606@gmail.com>
2024-08-04 21:23:38 +0300

    add-check-room

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',[\App\Http\Controllers\IndexController::class,'index']);
 Route::post('create-room',[\App\Http\Controllers\IndexController::class,'createRoom'])->name('create-room');
 Route::get('enter-room/{room}',[\App\Http\Controllers\IndexController::class,'enterRoom'])->name('enter-room');
+Route::get('enter-room2/{room}',[\App\Http\Controllers\IndexController::class,'enterRoom'])->name('enter-room');
 Route::post('enter-room-post/{room}',[\App\Http\Controllers\IndexController::class,'enterRoomPost'])->name('enter-room-post');
 
 Route::post('/room/{id}/delete', [\App\Http\Controllers\IndexController::class,'delete'])->name('room.delete');
 Route::post('/room/{id}/toggle', [\App\Http\Controllers\IndexController::class,'toggleStatus'])->name('room.toggle');
+Route::get('test',[\App\Http\Controllers\IndexController::class,'test']);
+Route::post('check-room',[\App\Http\Controllers\IndexController::class,'checkRoom']);
